7d8eca1f5c218932c4b559e910078b2b1013c645
konojunya <junya03dance@gmail.com>
2017-02-07 21:05:56 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # scraping-with-python
+
+PythonでScrapingのテストコード
+
+## 参考サイト
+[PythonでさくっとWebスクレイピングする (JavaScript読み込みにも対応しつつ)](http://qiita.com/beatinaniwa/items/72b777e23ef2390e13f8) 
---
+++ scraping_static_page.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+
+import urllib.request
+import chardet
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def scraping(url, output_name):
+	# get a HTML response
+	data = urllib.request.urlopen(url).read()
+
+	# エンコーディング判別とデコード
+	guess = chardet.detect(data)
+	html = data.decode(guess['encoding'])
+
+	# parse the response
+	soup = BeautifulSoup(html,"lxml")
+
+	# extract
+	title = soup.find("title").text
+	h1_data = soup.find("h1").text
+	
+	header = soup.find("head")
+	description = header.find("meta", attrs={"name": "description"})
+	description_content = description.attrs['content']
+
+	df = pd.DataFrame({
+		"title": [title],
+		"h1_text": [h1_data],
+		"descuription": [description_content]
+	})
+
+	df.to_csv(output_name,index=None)
+
+if __name__ == '__main__':
+	scraping("http://konojunya.com/", "konojunya.csv")
