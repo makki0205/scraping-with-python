063840c35ec94a5c330b249d2711112f5f4b7be2
katsuramaki taiki <llxo2_5oxll@icloud.com>
2017-02-08 09:20:14 +0900

    pep8 警告を排除

--- scraping_static_page.py
+++ scraping_static_page.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# -*- coding: utf-8 -*-
 
 import urllib.request
 import chardet
@@ -7,32 +7,33 @@ from bs4 import BeautifulSoup
 TARGET_URL = "http://konojunya.com/"
 CSV_PATH = "konojunya.csv"
 
+
 def scraping(url, output_name):
-	# get a HTML response
-	data = urllib.request.urlopen(url).read()
+    # get a HTML response
+    data = urllib.request.urlopen(url).read()
 
-	# エンコーディング判別とデコード
-	guess = chardet.detect(data)
-	html = data.decode(guess['encoding'])
+    # エンコーディング判別とデコード
+    guess = chardet.detect(data)
+    html = data.decode(guess['encoding'])
 
-	# parse the response
-	soup = BeautifulSoup(html,"lxml")
+    # parse the response
+    soup = BeautifulSoup(html, "lxml")
 
-	# extract
-	title = soup.find("title").text
-	h1_data = soup.find("h1").text
+    # extract
+    title = soup.find("title").text
+    h1_data = soup.find("h1").text
 
-	header = soup.find("head")
-	description = header.find("meta", attrs={"name": "description"})
-	description_content = description.attrs['content']
+    header = soup.find("head")
+    description = header.find("meta", attrs={"name": "description"})
+    description_content = description.attrs['content']
 
-	df = pd.DataFrame({
-		"title": [title],
-		"h1_text": [h1_data],
-		"descuription": [description_content]
-	})
+    df = pd.DataFrame({
+        "title": [title],
+        "h1_text": [h1_data],
+        "descuription": [description_content]
+    })
 
-	df.to_csv(output_name,index=None)
+    df.to_csv(output_name, index=None)
 
 if __name__ == '__main__':
-	scraping(TARGET_URL, CSV_PATH)
+    scraping(TARGET_URL, CSV_PATH)
